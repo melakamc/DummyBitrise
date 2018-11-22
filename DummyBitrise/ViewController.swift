8cce884ed70389aed25a6d8eef21a09d53705a40
Melaka <melakamc@gmail.com>
2018-11-22 14:59:04 +0800

    initial commit

---
+++ DummyBitrise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DummyBitrise
+//
+//  Created by Melaka Atalugamage on 22/11/18.
+//  Copyright © 2018 Rickton Pvt Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
